1157c047e04bab0613685bf202b21c7e689931c6
thiagotmb <tmb0710@gmail.com>
2017-03-05 15:59:17 -0500

    Fixing initializers deprecation

--- addon/initializers/initialize.js
+++ addon/initializers/initialize.js
@@ -9,16 +9,16 @@ import ParseUser from '../models/parse-user';
 @module initializers
 @class  initialize
 */
-export default function( container, app ) {
+export default function(app) {
   Adapter.reopen({
     applicationId : app.get( 'applicationId' ),
     restApiId     : app.get( 'restApiId' )
   });
 
-  container.register( 'adapter:-parse', Adapter );
-  container.register( 'serializer:-parse', Serializer );
-  container.register( 'transform:parse-date', DateTransform );
-  container.register( 'transform:parse-file', FileTransform );
-  container.register( 'transform:parse-geo-point', GeopointTransform );
-  container.register( 'model:parse-user', ParseUser );
+  app.register( 'adapter:-parse', Adapter );
+  app.register( 'serializer:-parse', Serializer );
+  app.register( 'transform:parse-date', DateTransform );
+  app.register( 'transform:parse-file', FileTransform );
+  app.register( 'transform:parse-geo-point', GeopointTransform );
+  app.register( 'model:parse-user', ParseUser );
 }
